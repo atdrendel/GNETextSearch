1174ddcb8eeb5b596c615c76ca48675a7c7173c8
Anthony Drendel <anthony@goneeast.com>
2015-08-31 16:37:58 +0200

    Initial Commit

---
+++ GNETextSearch/GNETextSearch.h
@@ -0,0 +1,19 @@
+//
+//  GNETextSearch.h
+//  GNETextSearch
+//
+//  Created by Anthony Drendel on 8/31/15.
+//  Copyright © 2015 Gone East LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GNETextSearch.
+FOUNDATION_EXPORT double GNETextSearchVersionNumber;
+
+//! Project version string for GNETextSearch.
+FOUNDATION_EXPORT const unsigned char GNETextSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GNETextSearch/PublicHeader.h>
+
+
