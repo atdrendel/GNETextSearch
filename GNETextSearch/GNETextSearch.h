7f8d8c11a687d99e6fff229c1d65b4bda38d9984
Anthony Drendel <anthony@goneeast.com>
2015-11-14 19:12:58 +0100

    Remove Cocoa-specific stuff from GNETextSearch.h

--- GNETextSearch/GNETextSearch.h
+++ GNETextSearch/GNETextSearch.h
@@ -6,14 +6,6 @@
 //  Copyright © 2015 Gone East LLC. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for GNETextSearch.
-FOUNDATION_EXPORT double GNETextSearchVersionNumber;
-
-//! Project version string for GNETextSearch.
-FOUNDATION_EXPORT const unsigned char GNETextSearchVersionString[];
-
 #import <GNETextSearch/GNETernaryTree.h>
 #import <GNETextSearch/GNEIntegerArray.h>
 
