30c9dcd08811795bbac600da30b1fa904737e2f1
Anthony Drendel <anthony@goneeast.com>
2016-06-22 22:42:13 -0500

    Remove unnecessary externs

--- GNETextSearch/String/stringbuf.h
+++ GNETextSearch/String/stringbuf.h
@@ -18,33 +18,33 @@ extern "C" {
 typedef struct tsearch_stringbuf * tsearch_stringbuf_ptr;
 
 /// Creates an empty mutable string. Returns a pointer to the mutable string if successful, otherwise NULL.
-extern tsearch_stringbuf_ptr tsearch_stringbuf_init(void);
+tsearch_stringbuf_ptr tsearch_stringbuf_init(void);
 
 /// Creates a mutable string containing the specified C char array.
 /// Returns a pointer to the mutable string if successful, otherwise NULL.
 /// The length parameter refers to the number of chars in cString, but should not include
 /// the null terminator.
-extern tsearch_stringbuf_ptr tsearch_stringbuf_init_with_cstring(const char *cString, const size_t length);
+tsearch_stringbuf_ptr tsearch_stringbuf_init_with_cstring(const char *cString, const size_t length);
 
-extern void tsearch_stringbuf_free(tsearch_stringbuf_ptr ptr);
+void tsearch_stringbuf_free(tsearch_stringbuf_ptr ptr);
 
 /// Returns the length of the mutable string. The length does not include space for a null terminator.
-extern size_t tsearch_stringbuf_get_len(tsearch_stringbuf_ptr ptr);
+size_t tsearch_stringbuf_get_len(tsearch_stringbuf_ptr ptr);
 
 /// Returns the char at the specified index of the mutable string.
 /// Returns '\0' if the index is past the bounds of the string or if the mutable string is NULL.
-extern char tsearch_stringbuf_get_char_at_idx(tsearch_stringbuf_ptr ptr, size_t index);
+char tsearch_stringbuf_get_char_at_idx(tsearch_stringbuf_ptr ptr, size_t index);
 
 /// Appends the specified C char array into the mutable string. Returns 1 if successful, otherwise 0.
 /// The length parameter refers to the number of chars in cString, but should not include
 /// the null terminator.
-extern result tsearch_stringbuf_append_cstring(tsearch_stringbuf_ptr ptr, const char *cString, const size_t length);
+result tsearch_stringbuf_append_cstring(tsearch_stringbuf_ptr ptr, const char *cString, const size_t length);
 
 /// Returns a null-terminated char representation of the mutable string's contents.
 /// The returned char array must be freed by the caller.
-extern const char * tsearch_stringbuf_copy_cstring(tsearch_stringbuf_ptr ptr);
+const char * tsearch_stringbuf_copy_cstring(tsearch_stringbuf_ptr ptr);
 
-extern void tsearch_stringbuf_print(tsearch_stringbuf_ptr ptr);
+void tsearch_stringbuf_print(tsearch_stringbuf_ptr ptr);
 
 #ifdef __cplusplus
 }
